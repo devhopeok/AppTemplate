5d9c71aafba31b659b5cc1da70db95da74db644f
Henry <mono_devok1111@protonmail.com>
2017-10-10 03:49:55 +0100

    Update paragraph content

--- AppTemplate/www/js/settings.js
+++ AppTemplate/www/js/settings.js
@@ -46,8 +46,8 @@ angular.module('starter.settings', [])
                         We help connect people to what they need to start a business here, grow a business here -- or even move a business to Carlisle. <br /><br />\
                         We plan, host, and partner in diverse networking and professional-development events.<br /><br />\
                         We foster dialogue and communication with our members and community partners about how we can better support them. <br /></br />\
-                        We offer exclusive advertising opportunities to members that showcase their business. ... <br /><br />\
-                        and much, much more!";
+                        We offer exclusive advertising opportunities to members that showcase their business.<br /><br />\
+                        ... and much, much more!";
     //var tracker_id = "UA-84899403-32";
     var tracker_id = "UA-84899403-1111";
 
